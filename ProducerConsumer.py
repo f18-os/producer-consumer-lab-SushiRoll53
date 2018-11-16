a6838e63918ba398e0922021194342b2d6e6be4e
SushiRoll <brfigueroa@miners.utep.edu>
2018-11-15 22:44:17 -0700

    First working prototype

--- ProducerConsumer.py
+++ ProducerConsumer.py
@@ -1,44 +1,90 @@
 from threading import Thread, Condition
 import time
 import random
+import cv2
+import numpy as np
+import base64
+import queue
 
-queue = []
+queue =[]
 MAX_NUM = 10
 condition = Condition()
 
-class ProducerThread(Thread):
+class Producer(Thread):
     def run(self):
-        nums = range(5)
         global queue
-        while True:
+        global fileName
+        # Initialize frame count 
+        count = 0
+        # open video file
+        vidcap = cv2.VideoCapture(fileName)
+        # read first image
+        success,image = vidcap.read()
+        
+        while success:
             condition.acquire()
             if len(queue) == MAX_NUM:
-                print "Queue full, producer is waiting"
+                print ("Queue full, producer is waiting")
                 condition.wait()
-                print "Space in queue, Consumer notified the producer"
-            num = random.choice(nums)
-            queue.append(num)
-            print "Produced", num
+                print ("Space in queue, Consumer notified the producer")
+
+            success, jpgImage = cv2.imencode('.jpg', image)
+
+            jpgAsText = base64.b64encode(jpgImage)
+
+            queue.append(jpgAsText)
+
+            success,image = vidcap.read()
+                
+            # num = random.choice(nums)
+            # queue.append(num)
+            print('Reading frame {} {}'.format(count, success))
+            count += 1
             condition.notify()
             condition.release()
             time.sleep(random.random())
 
 
-class ConsumerThread(Thread):
+class Consumer(Thread):
     def run(self):
         global queue
+        count = 0
         while True:
             condition.acquire()
             if not queue:
-                print "Nothing in queue, consumer is waiting"
+                print ("Nothing in queue, consumer is waiting")
                 condition.wait()
-                print "Producer added something to queue and notified the consumer"
-            num = queue.pop(0)
-            print "Consumed", num
+                print ("Producer added something to queue and notified the consumer")
+
+            frameAsText = queue.pop(0)
+            
+            jpgRawImage = base64.b64decode(frameAsText)
+
+            jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
+
+            img = cv2.imdecode( jpgImage ,cv2.IMREAD_UNCHANGED)
+
+            print("Displaying frame {}".format(count))
+
+            cv2.imshow("Video", img)
+            if cv2.waitKey(42) and 0xFF == ord("q"):
+                break
+
+            count += 1
             condition.notify()
             condition.release()
             time.sleep(random.random())
 
+        print("Finished displaying all frames")
+        # cleanup the windows
+        cv2.destroyAllWindows()
 
-ProducerThread().start()
-ConsumerThread().start()
+        
+
+fileName = 'clip.mp4'
+
+#queue = queue.Queue()
+
+Producer().start()
+
+Consumer().start()
